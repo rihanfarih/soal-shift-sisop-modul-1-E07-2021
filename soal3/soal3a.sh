484a767fd8cd8a1878ac19ccb0d24bcfbce03106
Fayha Syifa Qalbi <72442637+sfayhaa@users.noreply.github.com>
2021-03-26 20:44:03 +0700

    Update soal3a.sh

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+picture_dir="/home/bulqis/Desktop/sisop"
+log_dir="/home/bulqis/Desktop/sisop/Foto.log"
+
+for i in {1..23}
+do 
+    wget -a $log_dir "https://loremflickr.com/320/240/kitten" -O Koleksi_$i    
+done
+
+rdfind -deleteduplicates true 
+
+id=1;
+for i in $picture_dir
+do
+    baru1=$(echo $picture_dir"Koleksi_%d" "$id")
+if [ $i -lt 10 ]
+then mv -- "$i" "$0baru"
+else mv -- "$i" "$baru"
+fi
+done
+
+
