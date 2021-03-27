8cbfec9a73659a26d8522b6405c4228371e2df00
rihanfarih <55140308+rihanfarih@users.noreply.github.com>
2021-03-27 14:52:17 +0700

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+#no 1a
+
+error=$(grep -E "ERROR" syslog.log)
+info=$(grep -E "INFO" syslog.log)
+jumlaherror=$(grep -c 'ERROR' syslog.log)
+jumlahinfo=$(grep -c 'INFO' syslog.log)
+
+#echo $error
+#echo $info
+#echo $jumlaherror
+#echo $jumlahinfo
