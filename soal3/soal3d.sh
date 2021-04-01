e83bc3aa77aac3bb273db85d462422e743189383
Fayha Syifa Qalbi <72442637+sfayhaa@users.noreply.github.com>
2021-04-01 21:03:17 +0700

    Update soal3d.sh

--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-date_today=$(date +%d-%m-%Y)
-zip -P "$date_today" Koleksi.zip -r Kucing_* Kelinci_*
+picture_dir="/home/sfayha/Documents/SISOP/modul1/soal3"
+date_today=$(date +%m%d%Y)
+cd "$picture_dir"
+
+zip -P "$date_today" -m Koleksi.zip -r Kucing_* Kelinci_*
+
 
