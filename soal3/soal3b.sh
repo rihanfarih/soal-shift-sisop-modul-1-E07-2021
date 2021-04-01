2c9e91f5354bc6d83ad9e81e515c875e265fff68
Fayha Syifa Qalbi <72442637+sfayhaa@users.noreply.github.com>
2021-04-01 21:02:30 +0700

    Update soal3b.sh

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-
-bash ./soal3a.sh
+picture_dir="/home/sfayha/Documents/SISOP/modul1/soal3"
 sekarang=$(date +"%d-%m-%Y")
-mkdir "Kucing_$sekarang"
+#log_dir="/home/sfayha/Documents/SISOP/modul1/soal3/Foto.log"
 
+mkdir "$picture_dir"/"$sekarang"
+for i in {1..23}
+do 
+    if [ $i -lt 10 ]
+    then wget -a "$picture_dir"/Foto.log "https://loremflickr.com/320/240/kitten" -O "$picture_dir"/Koleksi_0"$i".png
+    else wget -a "$picture_dir"/Foto.log "https://loremflickr.com/320/240/kitten" -O "$picture_dir"/Koleksi_"$i".png
+fi    
+done
 
-mv ./Koleksi_* ./Foto.log "Kucing_$sekarang"
+rdfind -deleteduplicates true "$picture_dir"
 
+id=1;
+cd "$picture_dir"
+for i in Koleksi_*.png
+do
+    #baru=$( $picture_dir"Koleksi_0%d" "$id")
+    #baru=$( $picture_dir"Koleksi_%d" "$id")
+if [ $id -lt 10 ]
+    then mv -- "$i" Koleksi_0"$id"
+    else 
+        mv -- "$i" Koleksi_"$id"
+fi
+    let id=$id+1
+done
+
+mv ./Koleksi_* ./Foto.log "$picture_dir"/"$sekarang"
